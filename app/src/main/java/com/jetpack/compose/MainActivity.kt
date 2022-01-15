fb629b855c0fd9bb07302078ad311d4480fe06fa
Aung Thiha <mr.aungthiha@gmail.com>
2022-01-15 22:34:26 +0700

    state in composable

--- app/src/main/java/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/com/jetpack/compose/MainActivity.kt
@@ -3,12 +3,10 @@ package com.jetpack.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,23 +26,48 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Surface(color = MaterialTheme.colors.background) {
-        names.forEach { name ->
-            Greeting(name)
+        Column(modifier = Modifier.padding(vertical = 4.dp)) {
+            names.forEach { name ->
+                Greeting(name)
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello,")
-            Text(text = name)
+
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .padding(bottom = extraPadding)
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                val text = if (expanded.value) "Show less" else "Show more"
+                Text(text = text)
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicsTheme {
